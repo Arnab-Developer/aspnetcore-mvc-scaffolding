d8661ce253a4e47960f47a1c5c2cc66691097e34
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-04-04 11:17:16 +0530

    Add project files.

---
+++ WebApplication3/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Student name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Student subject")]
+        public string Subject { get; set; }
+    }
+}
